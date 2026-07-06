b70aca6a217548cdc09340c229de8df036f899f0
MUKIL KARKI M <mukilkarkimail@gmail.com>
2026-07-06 13:47:21 +0530

    update app/sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,6 +2,8 @@ import type { MetadataRoute } from "next";
 import { site } from "@/data/site";
 import { posts } from "@/data/blog";
 
+export const dynamic = 'force-dynamic'
+
 export default function sitemap(): MetadataRoute.Sitemap {
   const staticRoutes: MetadataRoute.Sitemap = [
     {
